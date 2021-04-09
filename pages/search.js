15fbc17969d749541d50855ddeb65862b823057b
JacobMason83 <jmason062083@gmail.com>
2021-04-09 17:13:27 -0400

    added env files to the project

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
-import {API_KEY, CONTEXT_KEY} from '../keys'
+
 import Response  from '../Response.js'
 import { useRouter } from 'next/router'
 import SearchResults from "../components/SearchResults";
@@ -34,7 +34,7 @@ export async function getServerSideProps(context) {
   const useDummyData = false;
   const startIndex = context.query.start || "0"
   const data = useDummyData ? Response : await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
   ).then((response) => response.json());
   // After the SERVER has rendered... Pass the results to the client
   return {
