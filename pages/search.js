170838d31d80df088b19abad7963566c0c42790c
JacobMason83 <jmason062083@gmail.com>
2021-04-09 16:24:48 -0400

    added styles to search results page

--- pages/search.js
+++ pages/search.js
@@ -2,13 +2,18 @@ import Head from "next/head";
 import Header from "../components/Header";
 import {API_KEY, CONTEXT_KEY} from '../keys'
 import Response  from '../Response.js'
+import {useRouter } from 'next/router'
+import SearchResults from "../components/SearchResults";
+
+
 
 function Search({ results }) {
- 
+ const router = useRouter();
+ console.log(results)
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} -Google Search</title>
         <link rel="icon" href="/favicon.io" />
       </Head>
 
@@ -17,6 +22,8 @@ function Search({ results }) {
         
       </Header>
       {/* Search Results */}
+      <SearchResults results={results} />
+
     </div>
   );
 }
@@ -25,8 +32,9 @@ export default Search;
 
 export async function getServerSideProps(context) {
   const useDummyData = false;
+  const startIndex = context.query.start || "0"
   const data = useDummyData ? Response : await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
+    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
   ).then((response) => response.json());
   // After the SERVER has rendered... Pass the results to the client
   return {
