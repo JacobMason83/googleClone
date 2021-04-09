5271c1a4027d4fa3d08b65cf24d1d71bb9a9c2f3
JacobMason83 <jmason062083@gmail.com>
2021-04-09 15:58:30 -0400

    added async function to allow serverside rendering of dummydata or custom search

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import {API_KEY, CONTEXT_KEY} from '../keys'
+import Response  from '../Response.js'
 
-function Search() {
+function Search({ results }) {
+ 
   return (
     <div>
       <Head>
@@ -9,7 +12,7 @@ function Search() {
         <link rel="icon" href="/favicon.io" />
       </Head>
 
-      {/* Header */}
+     
       <Header>
         
       </Header>
@@ -19,3 +22,16 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyData = false;
+  const data = useDummyData ? Response : await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
+  ).then((response) => response.json());
+  // After the SERVER has rendered... Pass the results to the client
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
